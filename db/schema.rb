8db608cf1181c6b45d4f08ad0125f74fd441157c
Joel C. Witherspoon <joel.witherspoon@gmail.com>
2014-07-17 11:57:53 -0700

    Updated Gem file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714174556) do
+ActiveRecord::Schema.define(version: 20140716005401) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140714174556) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
