f2d1e0da726d6a43b5fda72b865b4ee05624820c
Joel C. Witherspoon <joel.witherspoon@gmail.com>
2014-07-17 17:43:49 -0700

    recreating the db and fixing errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+
+  #Update products controller for strong params
+  #def product_params
+  #  params.require(:product).permit(:title, :description, :image, :price)
+  #end
+
   # GET /products
   # GET /products.json
   def index
@@ -71,4 +77,6 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716005401) do
+ActiveRecord::Schema.define(version: 20140717203937) do
 
   create_table "products", force: true do |t|
     t.string   "title"
